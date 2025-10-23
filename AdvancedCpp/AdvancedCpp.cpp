187cecfa815396107319b5ca6055de20e83c922c
MohamedOuhami <mohamed.ouhami2001@gmail.com>
2025-10-23 10:44:23 +0100

    Started Learning

---
+++ AdvancedCpp/AdvancedCpp.cpp
@@ -0,0 +1,12 @@
+﻿// AdvancedCpp.cpp : Defines the entry point for the application.
+//
+
+#include "AdvancedCpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
